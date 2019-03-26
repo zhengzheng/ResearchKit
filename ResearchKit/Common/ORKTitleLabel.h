4dff6b8fc02c0fab4cdc4849a9730acd69bbd415
Akshay <akshay@Akshays-macBook-Pro-2.local>
2019-03-26 16:49:41 -0400

    adding ORKTitleLabel

---
+++ ResearchKit/Common/ORKTitleLabel.h
@@ -0,0 +1,17 @@
+//
+//  ORKTitleLabel.h
+//  ResearchKit
+//
+//  Created by Akshay on 3/26/19.
+//  Copyright © 2019 researchkit.org. All rights reserved.
+//
+
+#import <ResearchKit/ResearchKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ORKTitleLabel : ORKLabel
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ResearchKit/Common/ORKTitleLabel.m
@@ -0,0 +1,21 @@
+//
+//  ORKTitleLabel.m
+//  ResearchKit
+//
+//  Created by Akshay on 3/26/19.
+//  Copyright © 2019 researchkit.org. All rights reserved.
+//
+
+#import "ORKTitleLabel.h"
+
+@implementation ORKTitleLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
