f349e19ace85139838735cd699e789e665e28e03
Srinath Tupil Muralidharan <srinath_muralidharan@apple.com>
2019-04-22 13:13:05 -0700

    adding test case for showsProgress

--- ResearchKitTests/ORKStepTests.m
+++ ResearchKitTests/ORKStepTests.m
@@ -192,6 +192,7 @@
     [step setTitle:@"Title"];
     [step setText:@"Text"];
     [step setTask:task];
+    step.showsProgress = NO;
     
     ORKStepViewController *controller = [step instantiateStepViewControllerWithResult:result];
     
@@ -203,6 +204,7 @@
     XCTAssertEqual([controller step], step);
     XCTAssertEqual([step stepViewControllerClass], [ORKStepViewController class]);
     XCTAssertEqual([step isRestorable], YES);
+    XCTAssertEqual([step showsProgress], NO);
     XCTAssert([step.identifier isEqualToString:@"STEP"]);
     XCTAssert([step isEqual:step]);
     XCTAssertEqual([step requestedPermissions], ORKPermissionNone);
