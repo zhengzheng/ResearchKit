d944a654170cff69cd6294b4af1bf53f4f8a00fd
Oscar San Juan <osanjuan@apple.com>
2019-04-22 08:17:50 -0700

    Changed copyright to 2019 Removed commented out import statement

--- ResearchKitTests/ORKPasscodeViewControllerTests.swift
+++ ResearchKitTests/ORKPasscodeViewControllerTests.swift
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2015, Apple Inc. All rights reserved.
+ Copyright (c) 2019, Apple Inc. All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification,
  are permitted provided that the following conditions are met:
@@ -31,7 +31,6 @@
 import XCTest
 @testable import ResearchKit
 import ResearchKit.Private
-//import ResearchKit.Internal
 
 class ORKPasscodeViewControllerTests: XCTestCase {
     
