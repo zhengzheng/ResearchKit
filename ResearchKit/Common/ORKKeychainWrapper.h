7a64620533f2e55fe23ca3ed2f44e790b82a4c0f
Akshay <akshay@Akshays-macBook-Pro-2.local>
2019-05-02 15:39:39 -0700

    undoing modification

--- ResearchKit/Common/ORKKeychainWrapper.h
+++ ResearchKit/Common/ORKKeychainWrapper.h
@@ -42,8 +42,6 @@ NS_ASSUME_NONNULL_BEGIN
 ORK_CLASS_AVAILABLE
 @interface ORKKeychainWrapper : NSObject
 
-+ (nullable id<NSSecureCoding>)valueForKey:(NSString *)key NS_UNAVAILABLE;
-
 /**
  Sets the given object in the keychain for the provided key.
  
